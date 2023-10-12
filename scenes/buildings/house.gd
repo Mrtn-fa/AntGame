9280220ebd5ae331075a6a9b44fbecd51972f9a7
kenia <kenia.castillo@ug.uchile.cl>
2023-10-12 11:58:26 -0300

    feat(ant hill timer): added ant hill timer and two positions for ant spawn

--- scenes/buildings/house.gd
+++ scenes/buildings/house.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-@onready var sprite = $Sprite2D
-@onready var nodo2d = $Node2D
+@onready var sprite = $AntHill
+@onready var nodo2d = $Position1
+@onready var position2 = $Position2
+@onready var pos = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,15 +12,36 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$TimeRemaining.text = "%s" % roundf($Timer.time_left)
+#	Debug.dprint($Timer.time_left)
 	pass
 
 func _input(event):
-	var posicion = nodo2d.global_position
+	
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		Debug.dprint(get_local_mouse_position())
+#		Debug.dprint(get_local_mouse_position())
 #		Debug.dprint(to_local(event.position))
-		if sprite.get_rect().has_point(get_local_mouse_position()):
-			Util.main.spawn_unit(posicion, null)
-			Debug.dprint("You clicked on this Sprite")
-			Debug.dprint("La posición del nodo 2D es:")
-			Debug.dprint(posicion)
+		if sprite.get_rect().has_point(get_local_mouse_position()) and $Timer.time_left==0:
+			$Timer.start()
+			$TimeRemaining.show()
+#			Util.main.spawn_unit(posicion, null)
+#			Debug.dprint("You clicked on this Sprite")
+#			Debug.dprint("La posición del nodo 2D es:")
+#			Debug.dprint(posicion)
+
+
+func _on_timer_timeout():
+	if pos==0:
+		var posicion = nodo2d.global_position
+#		Debug.dprint("timer timeout")
+#		Debug.dprint(posicion)
+		$TimeRemaining.hide()
+		Util.main.spawn_unit(posicion, null)
+		pos += 1
+	else:
+		var posicion = position2.global_position
+#		Debug.dprint(posicion)
+		$TimeRemaining.hide()
+		Util.main.spawn_unit(posicion, null)
+		pos -= 1
+	
