76122672658c7d300cd2aa022b27384729bfb6c8
kenia <kenia.castillo@ug.uchile.cl>
2023-11-14 11:07:19 -0300

    fix(main menu): fixed main menu

---
+++ scenes/ui/credits.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var MainMenuScene : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_packed(MainMenuScene)
