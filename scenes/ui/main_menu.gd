dd05be5830e9f67ad165513d39ec640361720e88
kenia <kenia.castillo@ug.uchile.cl>
2023-11-21 11:48:26 -0300

    feat(main menu): added basic main menu

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_jugar_pressed():
+	get_tree().change_scene_to_file("res://scenes/lobby.tscn")
+
+
+func _on_creditos_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/credits_menu.tscn")
+
+
+func _on_salir_pressed():
+	get_tree().quit()
