c3329f64dde64f52dd4c3e46777a738ab5340cb9
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-12-12 16:28:22 -0300

    adds controls screen to main menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -12,3 +12,7 @@ func _on_creditos_pressed():
 
 func _on_salir_pressed():
 	get_tree().quit()
+
+
+func _on_controles_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/control_menu.tscn")
