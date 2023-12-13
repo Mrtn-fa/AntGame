8ec32e8dbbc141dfb34087ee46dbef21bc2d6acf
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-12-12 21:55:56 -0300

    adds building progress bar

--- scenes/buildings/ghosts/ghost.gd
+++ scenes/buildings/ghosts/ghost.gd
@@ -1,2 +1,14 @@
 class_name Ghost extends Building
 
+
+var build_level = 0
+var build_max = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$TimeRemaining.max_value = build_max
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$TimeRemaining.value = build_level
